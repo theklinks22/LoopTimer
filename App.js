38aaf3a54a5e9f470b7e2aff22f19de7b10c85d9
Andrew <klinks22@gmail.com>
2021-02-22 17:46:41 -0500

    Add a button

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+
+const test = (val) => {
+
+}
 
 export default function App() {
   return (
+    <>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
+      <TextInput >
+        Insert
+      </TextInput>
+      <TouchableOpacity onPress={() => alert('Hello, world!')} style={styles.button}>
+        <Text style={styles.buttonText}>Start Timer</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
+    </>
   );
 }
 
@@ -18,4 +30,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }, 
 });
